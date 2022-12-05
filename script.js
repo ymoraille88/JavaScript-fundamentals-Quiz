5b2c2641850133555962eb060a563c566e2a3593
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2022-12-05 18:59:04 -0500

    setup  variable questions

---
+++ script.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        Question1: "",
+        answers: {
+            a: ""  ,
+            b:  "", 
+            c:  "" ,
+            d: ""  ,
+        },
+        correct: " ",
+    },
+    {
+        Question2: " ",
+        answers: {
+            a:  "" ,
+            b:  "" ,
+            c: ""  ,
+            d: ""  ,
+        },
+        correct: " ",
+    },
+    {
+        Question3: " ",
+        answers: {
+            a:  ""  ,
+            b:  ""  ,
+            c:   "" ,
+   {,
+    {
+    correct: " ",
+   }
+];

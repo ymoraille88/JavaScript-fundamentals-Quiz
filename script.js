7ba35262b46c7e0bf5f2da02d9b554147c69979b
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2022-12-10 19:15:59 -0500

    setup Local Storage

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-var questions = [
-    {
-        Question1: "what is JavaScript?",
-        answers: {
-            a: "does the same as HTML.",
-            b: "is a special mechanism we can find on the web.",
-            c: "is a programming language.",
-            d: "is a language derived from the spanish language",
-        },
-        correct: "c",
-    },
-    {
-        Question2: "whats a string in Javascript?",
-        answers: {
-            a: "its a way to connect with css",
-            b: "its a way to connect with html",
-            c: "its a way to connect with jQuery ",
-            d: "a string of sequence of one or more characters that may consist of letters, numbers or symbols",
-        },
-        correct: "d ",
-    },
-    {
-        Question3: "whats is an array in JavaScript? ",
-        answers: {
-            a: "used to store multiple values in a single variable. ",
-            b: "a sequence of one or more characters that may consist of letters, numbers or symbols",
-            c: "its a way to connect with jQuery",
-        },
-        correct: "a",
-    }
-];
+// var questions = [
+//     {
+//         Question1: "what is JavaScript?",
+//         answers: {
+//             a: "does the same as HTML.",
+//             b: "is a special mechanism we can find on the web.",
+//             c: "is a programming language.",
+//             d: "is a language derived from the spanish language",
+//         },
+//         correct: "c",
+//     },
+//     {
+//         Question2: "whats a string in Javascript?",
+//         answers: {
+//             a: "its a way to connect with css",
+//             b: "its a way to connect with html",
+//             c: "its a way to connect with jQuery ",
+//             d: "a string of sequence of one or more characters that may consist of letters, numbers or symbols",
+//         },
+//         correct: "d ",
+//     },
+//     {
+//         Question3: "whats is an array in JavaScript? ",
+//         answers: {
+//             a: "used to store multiple values in a single variable. ",
+//             b: "a sequence of one or more characters that may consist of letters, numbers or symbols",
+//             c: "its a way to connect with jQuery",
+//         },
+//         correct: "a",
+//     }
+// ];
 
 // function showQuestions(questions) {
 

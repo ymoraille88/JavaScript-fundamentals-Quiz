9f3591acac719ae048ad678e5b300b82c6cf4a42
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2023-02-13 19:27:20 -0500

    added eventlistener to the questions "

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,73 @@
-// var questions = [
-//     {
-//         Question1: "what is JavaScript?",
-//         answers: {
-//             a: "does the same as HTML.",
-//             b: "is a special mechanism we can find on the web.",
-//             c: "is a programming language.",
-//             d: "is a language derived from the spanish language",
-//         },
-//         correct: "c",
-//     },
-//     {
-//         Question2: "whats a string in Javascript?",
-//         answers: {
-//             a: "its a way to connect with css",
-//             b: "its a way to connect with html",
-//             c: "its a way to connect with jQuery ",
-//             d: "a string of sequence of one or more characters that may consist of letters, numbers or symbols",
-//         },
-//         correct: "d ",
-//     },
-//     {
-//         Question3: "whats is an array in JavaScript? ",
-//         answers: {
-//             a: "used to store multiple values in a single variable. ",
-//             b: "a sequence of one or more characters that may consist of letters, numbers or symbols",
-//             c: "its a way to connect with jQuery",
-//         },
-//         correct: "a",
-//     }
-// ];
+// var generateBtn = document.querySelector('.choices');
+var timeEl = document.querySelector('.time');
+var questionEl = document.querySelector('.question');
+var bodyEl = document.querySelector('.container');
+var resultEl = document.querySelector('.result');
 
-// function showQuestions(questions) {
 
-//     var output = []
-//     var answers;
 
-//     for (var i = 0; i < questions.length; i++) {
 
-//         answers = [];
+var questions = [
+    {
+        question: "what is JavaScript?",
+        answers: [
+            "does the same as HTML.",
+            "is a special mechanism we can find on the web.",
+            "is a programming language.",
+        ],
+
+
+
+        correct: "is a programming language",
+    },
+    {
+        question: "what is a string in Javascript?",
+        answers: [
+            "its a way to connect with css",
+            "its a way to connect with html",
+            "a string is  sequence of one or more characters that may consist of letters, numbers or symbols",
+        ],
+
+        correct: "a string is  sequence of one or more characters that may consist of letters, numbers or symbols",
+    },
+    {
+        question: "whats is an array in JavaScript? ",
+        answers: [
+            "used to store multiple values in a single variable. ",
+            "a sequence of one or more characters that may consist of letters, numbers or symbols",
+            "its a way to connect with jQuery",
+
+        ],
+
+        correct: "a",
+    }
+];
+
+var displayQuestion = function () {
+    document.querySelector('.questions').textContent = questions[0].question
+    for (var i = 0; i < 3; i++) {
+        var button = document.createElement('button')
+        button.textContent = questions[0].answers[i]
+        button.addEventListener('click', function () {
+        displayQuestion()
+        })
+        document.querySelector('.choices').appendChild(button)
+        
+    }
+
+};
+
+
+displayQuestion();
+
+
+
+
+
+
+
+
+
+// generateBtn.addEventListener("click", start );
+
+
